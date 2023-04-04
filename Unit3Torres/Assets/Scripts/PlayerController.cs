347579467f4206ca171d30819bf7f4f5513168fa
Victor T <116121215+littlevixplanet@users.noreply.github.com>
2023-04-04 15:11:52 -0700

    Bonus features
    Completed all bonus features except the intro cutscene

--- Unit3Torres/Assets/Scripts/PlayerController.cs
+++ Unit3Torres/Assets/Scripts/PlayerController.cs
@@ -15,6 +15,9 @@ public class PlayerController : MonoBehaviour
     public AudioClip jumpSound;
     public AudioClip crashSound;
     private AudioSource playerAudio;
+    public bool doubleJumpUsed = false;
+    public float doubleJumpForce;
+    public bool doubleSpeed = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,6 +37,24 @@ public class PlayerController : MonoBehaviour
             playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
             playerAudio.PlayOneShot(jumpSound, 1.0f);
+            doubleJumpUsed = false;
+        }
+        else if(Input.GetKeyDown(KeyCode.Space) && !isOnGround && !doubleJumpUsed)
+        {
+            doubleJumpUsed = true;
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+            playerAnim.Play("Running_Jump",3, 0f);
+            playerRb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
+        }
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            doubleSpeed = true;
+            playerAnim.SetFloat("Speed_Multiplier", 2.0f);
+        }
+        else if (doubleSpeed)
+        {
+            doubleSpeed = false;
+            playerAnim.SetFloat("Speed_Multiplier", 1.0f);
         }
     }
     private void OnCollisionEnter(Collision collision)
